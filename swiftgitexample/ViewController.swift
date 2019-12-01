cc8460fa845847a1793e600656d4d6e0dc687d70
Emily Rogers <emilyrogers@mail.usf.edu>
2019-12-01 08:48:12 -0500

    Initial Commit

---
+++ swiftgitexample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftgitexample
+//
+//  Created by Emily Rogers on 12/1/19.
+//  Copyright © 2019 Emily Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
